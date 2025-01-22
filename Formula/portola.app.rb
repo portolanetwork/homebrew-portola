1e5431602ee39bfbfd103bb233a45ebe0c9d237b
portolanetwork <sami.malik@portolanetwork.io>
2025-01-21 23:58:14 -0800

    Brew formula update for portola version v0.6.24

--- Formula/portola.app.rb
+++ Formula/portola.app.rb
@@ -5,29 +5,29 @@
 class PortolaApp < Formula
   desc ""
   homepage "https://github.com/portolanetwork/portola"
-  version "0.6.23"
+  version "0.6.24"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/portolanetwork/portola-release/releases/download/v0.6.23/portola_Darwin_x86_64.tar.gz"
-    sha256 "3e344ff2587f46faba94f523230ef6bc4329992db5e4c1a9d919235309fe1bec"
+    url "https://github.com/portolanetwork/portola-release/releases/download/v0.6.24/portola_Darwin_x86_64.tar.gz"
+    sha256 "6e0873466f8431dfa3a0b882b99f6763558fb3c5c6ea374c915e179dd84b2c7b"
 
     def install
       bin.install "portd"
       bin.install "portctl"
-      prefix.install Dir["cmd/portd/config/prod-usw1/resources"]
-      prefix.install "cmd/portd/config/prod-usw1/deployment.yaml"
+      prefix.install Dir["cmd/portd/deployment-assets/prod-usw1/resources"]
+      prefix.install "cmd/portd/deployment-assets/prod-usw1/deployment.yaml"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/portolanetwork/portola-release/releases/download/v0.6.23/portola_Darwin_arm64.tar.gz"
-    sha256 "55647399b94236fb25e8b64ec2bcd189a323a89169467d68cb0dbd14cbd13c30"
+    url "https://github.com/portolanetwork/portola-release/releases/download/v0.6.24/portola_Darwin_arm64.tar.gz"
+    sha256 "a000f9a21ad666f2b39cb9e549f93444124ac5b173630c677addba1433fc40e5"
 
     def install
       bin.install "portd"
       bin.install "portctl"
-      prefix.install Dir["cmd/portd/config/prod-usw1/resources"]
-      prefix.install "cmd/portd/config/prod-usw1/deployment.yaml"
+      prefix.install Dir["cmd/portd/deployment-assets/prod-usw1/resources"]
+      prefix.install "cmd/portd/deployment-assets/prod-usw1/deployment.yaml"
     end
   end
 
