e04bdfe2842ad7f7488556ae20187441f90619d9
portolanetwork <sami.malik@portolanetwork.io>
2025-06-03 23:58:15 -0700

    Brew formula update for portola version v0.7.279

--- Formula/portola.app.rb
+++ Formula/portola.app.rb
@@ -5,12 +5,12 @@
 class PortolaApp < Formula
   desc ""
   homepage "https://github.com/portolanetwork/portola"
-  version "0.7.114"
+  version "0.7.279"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/portolanetwork/portola-release/releases/download/v0.7.114/portola_Darwin_x86_64.tar.gz"
-    sha256 "7a7abc78f872ea6c42e7996c57595e5b80dc4262661a63f5ae7716b49b3668d7"
+    url "https://github.com/portolanetwork/portola-release/releases/download/v0.7.279/portola_Darwin_x86_64.tar.gz"
+    sha256 "ec2d48ada06ab11910b9d4c1bbfbef4cbd89db937ea9b0a798255fb9992e3e2c"
 
     def install
       bin.install "portd"
@@ -20,8 +20,8 @@ class PortolaApp < Formula
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/portolanetwork/portola-release/releases/download/v0.7.114/portola_Darwin_arm64.tar.gz"
-    sha256 "9c8d9d2b037f970b966072ac02459c39fa7394112241d5d3367207f55cdc7fa7"
+    url "https://github.com/portolanetwork/portola-release/releases/download/v0.7.279/portola_Darwin_arm64.tar.gz"
+    sha256 "46c9d5d23ff79fe49cff47a570e89f7125749209638c592275d280d7b69d1576"
 
     def install
       bin.install "portd"
