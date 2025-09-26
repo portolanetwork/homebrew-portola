dcbfe348dc263bb59ed9411c33b9ae0138ca7221
portolanetwork <sami.malik@portolanetwork.io>
2025-09-26 14:51:07 -0700

    Brew formula update for Portola version v0.8.43

--- Formula/portola.app.rb
+++ Formula/portola.app.rb
@@ -5,12 +5,12 @@
 class PortolaApp < Formula
   desc ""
   homepage "https://github.com/portolanetwork/portola"
-  version "0.7.114"
+  version "0.8.43"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/portolanetwork/portola-release/releases/download/v0.7.114/portola_Darwin_x86_64.tar.gz"
-    sha256 "7a7abc78f872ea6c42e7996c57595e5b80dc4262661a63f5ae7716b49b3668d7"
+    url "https://github.com/portolanetwork/portola-release/releases/download/v0.8.43/Portola_Darwin_x86_64.tar.gz"
+    sha256 "78687f42c2df2b6a881ea1407fd93b878d306497f371f201188aebf6ca95b039"
 
     def install
       bin.install "portd"
@@ -20,8 +20,8 @@ class PortolaApp < Formula
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/portolanetwork/portola-release/releases/download/v0.7.114/portola_Darwin_arm64.tar.gz"
-    sha256 "9c8d9d2b037f970b966072ac02459c39fa7394112241d5d3367207f55cdc7fa7"
+    url "https://github.com/portolanetwork/portola-release/releases/download/v0.8.43/Portola_Darwin_arm64.tar.gz"
+    sha256 "97f6e82a8b3a2fab719717173b4444513aebb4769c24dbab4ebaff4bb68ea4af"
 
     def install
       bin.install "portd"
@@ -34,7 +34,7 @@ class PortolaApp < Formula
   def caveats
     <<~EOS
       ****************************************************************
-      Installed Portola Staging Formula. To start the service run:
+      Installed Portola Formula. To start the service run:
         brew services start portola.app
       ****************************************************************
     EOS
